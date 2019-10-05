f1840d3974aee9373b28e4cb036b0ea45c816fa8
SanderKlootwijk <sander.k1007@kpnmail.nl>
2019-10-05 17:18:40 +0200

    Search test 2

--- js/search.js
+++ js/search.js
@@ -1,57 +1,24 @@
 
-jQuery(function() {
-  // Initialize lunr with the fields to be searched, plus the boost.
-  window.idx = lunr(function () {
-    this.field('titel');
-    this.field('schrijvers');
-    this.field('beschrijving', { boost: 10 });
-    this.field('apa');
+$(document).ready(function(){
+ $.ajaxSetup({ cache: false });
+ $('#search').keyup(function(){
+  $('#result').html('');
+  $('#state').val('');
+  var searchField = $('#search').val();
+  var expression = new RegExp(searchField, "i");
+  $.getJSON('data.json', function(data) {
+   $.each(data, function(key, value){
+    if (value.titel.search(expression) != -1 || value.schrijvers.search(expression) != -1 || value.beschrijving.search(expression) != -1 || value.jaartal.search(expression) != -1)
+    {
+     $('#result').append('<li class="list-group-item link-class"><img src="'+value.image+'" height="40" width="40" class="img-thumbnail" /> '+value.name+' | <span class="text-muted">'+value.location+'</span></li>');
+    }
+   });
   });
+ });
 
-  // Get the generated search_data.json file so lunr.js can search it locally.
-  window.data = $.getJSON('search_data.json');
-
-  // Wait for the data to load and add it to lunr
-  window.data.then(function(loaded_data){
-    $.each(loaded_data, function(index, value){
-      window.idx.add(
-        $.extend({ "id": index }, value)
-      );
-    });
-  });
-
-  // Event when the form is submitted
-  $("#site_search").submit(function(event){
-      event.preventDefault();
-      var query = $("#search_box").val(); // Get the value for the text field
-      var results = window.idx.search(query); // Get lunr to perform a search
-      display_search_results(results); // Hand the results off to be displayed
-  });
-
-  function display_search_results(results) {
-    var $search_results = $("#search_results");
-
-    // Wait for data to load
-    window.data.then(function(loaded_data) {
-
-      // Are there any results?
-      if (results.length) {
-        $search_results.empty(); // Clear any old results
-
-        // Iterate over the results
-        results.forEach(function(result) {
-          var item = loaded_data[result.ref];
-
-          // Build a snippet of HTML for this result
-          var appendString = '<li><a href="' + item.url + '">' + item.title + '</a></li>';
-
-          // Add the snippet to the collection of results.
-          $search_results.append(appendString);
-        });
-      } else {
-        // If there are no results, let the user know.
-        $search_results.html('<li>No results found.<br/>Please check spelling, spacing, yada...</li>');
-      }
-    });
-  }
+ $('#result').on('click', 'li', function() {
+  var click_text = $(this).text().split('|');
+  $('#search').val($.trim(click_text[0]));
+  $("#result").html('');
+ });
 });
