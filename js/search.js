d02f1fbdd1764a7c7e7f0220559c95494b524300
SanderKlootwijk <sander.k1007@kpnmail.nl>
2019-10-05 18:49:07 +0200

    Search test 2

--- js/search.js
+++ js/search.js
@@ -2,9 +2,23 @@ $(document).ready(function(){
   $('#search').keyup(function(){
     var searchValue = ($('#search').val()).trim();
     if(searchValue != '') {
-      $.getJSON('list.json', function(result){
+      $.getJSON('data.json', function(result){
         var list = result;
-        console.log(result);
+        var options = {
+          shouldSort: true,
+          threshold: 0.6,
+          location: 0,
+          distance: 100,
+          maxPatternLength: 32,
+          minMatchCharLength: 2,
+          keys: [
+            "titel",
+            "schrijver"
+          ]
+        };
+        var fuse = new Fuse(list, options);
+        var searchResult = fuse.search(searchValue);
+        console.log(searchResult)
       })
     }
   })
