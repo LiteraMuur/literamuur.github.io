fd075ca9a97d799ba865b44e1355f3cef902235a
SanderKlootwijk <sander.k1007@kpnmail.nl>
2019-10-05 18:07:48 +0200

    Search test 2

--- js/search.js
+++ js/search.js
@@ -7,9 +7,9 @@ $(document).ready(function(){
   var expression = new RegExp(searchField, "i");
   $.getJSON('data.json', function(data) {
    $.each(data, function(key, value){
-    if (value.name.search(expression) != -1 || value.location.search(expression) != -1)
+    if (value.titel.search(expression) != -1 || value.schrijver.search(expression) != -1)
     {
-     $('#result').append('<li class="list-group-item link-class"><img src="'+value.image+'" height="40" width="40" class="img-thumbnail" /> '+value.name+' | <span class="text-muted">'+value.location+'</span></li>');
+     $('#result').append('<li class="list-group-item link-class"><img src="'+value.image+'" height="40" width="40" class="img-thumbnail" /> '+value.titel+' | <span class="text-muted">'+value.schrijver+'</span></li>');
     }
    });
   });
