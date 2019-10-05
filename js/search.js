6f34c7f2d9520cb8fd0bf5e3afb2fbffebbac091
SanderKlootwijk <sander.k1007@kpnmail.nl>
2019-10-05 18:44:09 +0200

    Search test 2

--- js/search.js
+++ js/search.js
@@ -1,6 +1,11 @@
 $(document).ready(function(){
   $('#search').keyup(function(){
-    var searchValue = $('#search').val()
-    console.log(searchValue)
+    var searchValue = ($('#search').val()).trim();
+    if(searchValue != '') {
+      $.getJSON('list.json', function(result){
+        var list = result;
+        console.log(result);
+      })
+    }
   })
 })
