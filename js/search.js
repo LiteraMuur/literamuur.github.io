7b5a45dc9568b9fa5c771e8ba5a6c8cde880221f
SanderKlootwijk <sander.k1007@kpnmail.nl>
2019-10-05 18:37:26 +0200

    Search test 2

--- js/search.js
+++ js/search.js
@@ -1,23 +1,6 @@
 $(document).ready(function(){
- $.ajaxSetup({ cache: false });
- $('#search').keyup(function(){
-  $('#result').html('');
-  $('#state').val('');
-  var searchField = $('#search').val();
-  var expression = new RegExp(searchField, "i");
-  $.getJSON('data.json', function(data) {
-   $.each(data, function(key, value){
-    if (value.titel.search(expression) != -1 || value.schrijver.search(expression) != -1)
-    {
-     $('#result').append('<li class="list-group-item link-class"><img src="'+value.image+'" height="40" width="40" class="img-thumbnail" /> '+value.titel+' | <span class="text-muted">'+value.schrijver+'</span></li>');
-    }
-   });
-  });
- });
-
- $('#result').on('click', 'li', function() {
-  var click_text = $(this).text().split('|');
-  $('#search').val($.trim(click_text[0]));
-  $("#result").html('');
- });
-});
+  $('#search').keyup(function(){
+    var searchValue = $('#search').val()
+    console.log(searchValue)
+  })
+})
