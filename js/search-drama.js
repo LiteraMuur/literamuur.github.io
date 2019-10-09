dfde53c4739e5b1441e301e5f97b3a12b30ce3d5
Sander Klootwijk <sander.klootwijk@tutanota.com>
2019-10-09 22:02:31 +0200

    Search filter test

--- js/search-drama.js
+++ js/search-drama.js
@@ -6,7 +6,6 @@ $(document).ready(function(){
         var list = result;
         var options = {
           shouldSort: true,
-          tokenize: true,
           threshold: 0.4,
           location: 0,
           distance: 100,
@@ -20,12 +19,16 @@ $(document).ready(function(){
           ]
         };
         var fuse = new Fuse(list, options);
-        var searchResult = fuse.search('drama ' + searchValue);
+        var searchResult = fuse.search(searchValue);
         console.log(searchResult)
         if(searchResult.length > 0) {
           $('#results').empty();
           for(i = 0; i < searchResult.length; i++) {
+            if (searchResult.vakgebied = 'drama'){
             $('#results').append('<div class="res"><img class="coverfoto" src="images/covers/'+searchResult[i].id+'.jpg"><div class="informatie"><b>'+searchResult[i].titel+'</b> | <font color ="#5c5c5c">'+searchResult[i].schrijver+'</font><p>'+searchResult[i].beschrijving+'</p><a href="boeken/'+searchResult[i].id+'.html">>> Bekijk</a></div></div>')
+          }
+          else {
+          }
           }
 
         }
