4ed7cef6c16058cb7076bec0842d048f9412862e
Sander Klootwijk <sander.klootwijk@tutanota.com>
2019-11-20 12:20:13 +0100

    Update aardrijkskunde

---
+++ vakgebieden/aardrijkskunde/didactiek/bovenbouw/differentiatie.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+    var searchValue = 'bb-differentiatie';
+    if(searchValue != '') {
+      $.getJSON('/data.json', function(result){
+        var list = result;
+        var options = {
+          shouldSort: true,
+          threshold: 0,
+          location: 0,
+          distance: 100,
+          maxPatternLength: 32,
+          minMatchCharLength: 5,
+          keys: [
+            "subcategorie"
+          ]
+        };
+        var fuse = new Fuse(list, options);
+        var searchResult = fuse.search(searchValue);
+        console.log(searchResult)
+        if(searchResult.length > 0) {
+          $('#results').empty();
+          for(i = 0; i < searchResult.length; i++) {
+            $('#results').append('<div class="res"><img class="coverfoto" src="/images/covers/'+searchResult[i].id+'.jpg"><div class="informatie"><b>'+searchResult[i].titel+'</b> | <font color ="#5c5c5c">'+searchResult[i].schrijver+'</font><p>'+searchResult[i].beschrijving+'</p><a href="/boeken/'+searchResult[i].id+'.html">>> Bekijk</a></div></div>')
+          }
+
+        }
+        else {
+          $('#results').empty();
+          $('#results').html('<center><font><h3>Geen resultaten gevonden</h3></font></center>')
+        }
+      })
+    }
+    else {
+      $('#results').empty();
+      $('#results').html('')
+    }
+})
