f8b4c13e7ee03e1f23ca2e5a640120a23900ab1e
LiteraMuur <56129698+LiteraMuur@users.noreply.github.com>
2020-01-23 17:38:42 +0100

    Update search.js

--- js/search.js
+++ js/search.js
@@ -25,7 +25,7 @@ $(document).ready(function(){
         if(searchResult.length > 0) {
           $('#results').empty();
           for(i = 0; i < searchResult.length; i++) {
-            $('#results').append('<div class="res"> <div class="bb" style="visibility: '+searchResult[i].bb+';"><div class="bubbletext">6-8</div></div><div class="mb" style="visibility: '+searchResult[i].mb+';"><div class="bubbletext">3-5</div></div><div class="ob" style="visibility: '+searchResult[i].ob+';"><div class="bubbletext">1-2</div></div><img class="coverfoto" src="/images/covers/'+searchResult[i].id+'.jpg"><div class="informatie"><b>'+searchResult[i].titel+'</b> | <font color ="#5c5c5c">'+searchResult[i].schrijver+'</font><p>'+searchResult[i].beschrijving+'</p><a href="/boeken/'+searchResult[i].id+'.html">>> Bekijk</a></div></div>')
+            $('#results').append('<div class="res"> <div class="bubblebackground"><div class="bb" style="visibility: '+searchResult[i].bb+';"><div class="bubbletext">6-8</div></div></div> <div class="bubblebackground"><div class="mb" style="visibility: '+searchResult[i].mb+';"><div class="bubbletext">3-5</div></div></div> <div class="bubblebackground"><div class="ob" style="visibility: '+searchResult[i].ob+';"><div class="bubbletext">1-2</div></div></div><img class="coverfoto" src="/images/covers/'+searchResult[i].id+'.jpg"><div class="informatie"><b>'+searchResult[i].titel+'</b> | <font color ="#5c5c5c">'+searchResult[i].schrijver+'</font><p>'+searchResult[i].beschrijving+'</p><a href="/boeken/'+searchResult[i].id+'.html">>> Bekijk</a></div></div>')
           }
 
         }
