bdc908e8e8e3b00f8c08179874f56f8db0594f76
LiteraMuur <56129698+LiteraMuur@users.noreply.github.com>
2020-01-23 19:30:10 +0100

    Update search.js

--- js/search.js
+++ js/search.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
         var list = result;
         var options = {
           shouldSort: true,
-          threshold: 0.4,
+          threshold: 0.5,
           location: 0,
           distance: 100,
           maxPatternLength: 32,
@@ -22,7 +22,7 @@ $(document).ready(function(){
         var fuse = new Fuse(list, options);
         var searchResult = fuse.search(searchValue);
         console.log(searchResult)
-        if(searchResult.length > 0) {
+        if(searchResult.length > 3) {
           $('#results').empty();
           for(i = 0; i < searchResult.length; i++) {
             $('#results').append('<div class="res"><div class="bubblebackground"><div class="bb" style="visibility: '+searchResult[i].bb+';"><div class="bubbletext">6-8</div></div></div><div class="bubblebackground"><div class="mb" style="visibility: '+searchResult[i].mb+';"><div class="bubbletext">3-5</div></div></div><div class="bubblebackground"><div class="ob" style="visibility: '+searchResult[i].ob+';"><div class="bubbletext">1-2</div></div></div><img class="coverfoto" src="/images/covers/'+searchResult[i].id+'.jpg"><div class="informatie"><b>'+searchResult[i].titel+'</b> | <font color ="#5c5c5c">'+searchResult[i].schrijver+'</font><p>'+searchResult[i].beschrijving+'</p><a href="/boeken/'+searchResult[i].id+'.html">>> Bekijk</a></div></div>')
