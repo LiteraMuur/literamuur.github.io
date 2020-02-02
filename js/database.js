902fa1fbd941507f48c9eb82e183318e586360ee
Sander Klootwijk <sander.klootwijk@tutanota.com>
2020-02-02 16:55:59 +0100

    Update js

--- js/database.js
+++ js/database.js
@@ -1,17 +1,18 @@
-$(document).ready( function() {
-$.getJSON('/data.json',
-function(data) {
-  $("#Table").html(" FirstLastMiddle");
-  for (var i = 0; i < data.length; i++)
-      {
-          var tr="";
-            var td1=""+data[i]["titel"]+"";
-            var td2=""+data[i]["boek"]+"";
-            var td3=""+data[i]["schrijver"]+"";
-            var td4=""+data[i]["ob"]+"";
+$(function () {
 
-          $("#Table").append(tr+td1+td2+td3+td4);
-
-      }
+$.getJSON('/data.json', function(data) {
+    var table = $("#Table").empty();
+    $.each(data, function (i, member) {
+        $("<tr>", {
+            html: [
+                $("<td>", { html: member.name }),
+                $("<td>", { html: member.email }),
+                $("<td>", { html: member.phone }),
+                $("<td>", { html: member.id })
+            ],
+            appendTo: table
+        });
+    });
 });
+
 });
