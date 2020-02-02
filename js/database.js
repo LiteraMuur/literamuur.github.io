a028ebf901ac1807fbcf2526108721f6679d1696
Sander Klootwijk <sander.klootwijk@tutanota.com>
2020-02-02 17:12:07 +0100

    Update database table

--- js/database.js
+++ js/database.js
@@ -8,7 +8,9 @@ $.getJSON('/data.json', function(data) {
                 $("<td>", { html: boek.id }),
                 $("<td>", { html: boek.titel }),
                 $("<td>", { html: boek.schrijver }),
-                $("<td>", { html: boek.tags })
+                $("<td>", { html: boek.ob }),
+                $("<td>", { html: boek.mb }),
+                $("<td>", { html: boek.bb }),
             ],
             appendTo: table
         });
